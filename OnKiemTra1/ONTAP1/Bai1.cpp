0b5bbd0ff9d2e90bad6cf3f55ba730f50043d664
Nguyen The Van <114898603+zannngyn@users.noreply.github.com>
2024-10-27 16:20:01 +0700

    Update Bai1.cpp
    Code vip pro

--- OnKiemTra1/ONTAP1/Bai1.cpp
+++ OnKiemTra1/ONTAP1/Bai1.cpp
@@ -1,60 +1,74 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-void viewconfig(int n, int x[], char label[], string name[]){
-	for(int i = 1; i <= n; i++){
-		cout << label[x[i]-1] << "-" << name[x[i]-1] << " ";
+int countConfig=0;
+
+void ViewConfig(int n, int x[], char label[], string name[]){
+	for (int i=1; i<=n; i++){
+		cout<< label[x[i]-1]<<" - "<<name[x[i]-1]<<setw(5);
 	}
-	cout << endl;
+	cout<<endl;
+	countConfig++;
 }
 
-void nextconfig(int n, int x[], int i){
-	int k = n;
-	while(x[k] < x[i]) k--;
-	swap(x[i], x[k]);
-	int j = n; i++;
-	while(i <= j){
-		swap(x[i], x[j]);
-		i++; j--;
+void NextConfig(int n, int x[], int i){
+	int k=n;
+	while (x[k]<x[i]) k--;
+	swap(x[i],x[k]);
+	int j=n;
+	i++;
+	while (i<=j){
+		swap(x[i],x[j]);
+		i++;
+		j--;
 	}
 }
 
-void listconfig(int n, char label[], string name[]){
+void ListConfig(int n, char label[], string name[]){
 	int i;
 	int x[n+1];
-	for(int i = 1; i <= n; i++){
-		x[i] = i;
+	for(int i=1; i<=n; i++){
+		x[i]=i;
 	}
 	do{
-		viewconfig(n, x, label, name);
-		i = n-1;
-		while(i > 0 && x[i] > x[i+1]) i--;
-		if(i > 0) nextconfig(n, x, i);
-	}while(i > 0);
+		ViewConfig(n, x, label, name);
+		i=n-1;
+		while(i>0 && x[i]>x[i+1]){
+			i--;
+		}
+		if(i>0){
+			NextConfig(n, x, i);
+
+		}
+	}
+	while(i>0);
+	cout<<"Tong so Hoan vi:"<<countConfig;
 }
 
-void Try(int k, int n, int x[], int check[], char label[], string name[]){
-	for(int i = 1; i <= n; i++){
-		if(check[i] == 0){
-			x[k] = i;
-			if(n == k) viewconfig(n, x, label, name);
+void BackTracking(int k, int n, int x[], int check[], char label[], string name[]){
+	for (int i = 1; i<=n; i++){
+		if (check[i]==0){
+			x[k]=i;
+			if(n==k) ViewConfig(n,x,label,name);
 			else{
 				check[i] = 1;
-				Try(k+1, n, x, check, label, name);
-				check[i] = 0;
+				BackTracking(k+1,n,x,check,label,name);
+				check[i]=0;
 			}
 		}
 	}
+	
 }
 
+
 int main(){
-	int n = 4;
-	char label[]= {'A', 'B', 'C', 'D'};
-	string name[] = {"Tung", "Cuc", "Truc", "Mai"};
-//	listconfig(n, label, name);
+	int n=4;
+	char label[]={'A','B','C','D'};
+//	string name[]={"Hung","Cuc","Truc","Mai"};
+//	ListConfig(n, label, name);
 	
 	int x1[n+1] = {0}, check[n+1] = {0};
-//	for(int i = )
-	Try(1, n, x1, check, label, name);
+	BackTracking(1,n,x1,check,label,name);
+	cout<<"Tong so Hoan vi:"<<countConfig;
 }
+	
